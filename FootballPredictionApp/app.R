de2c2e7774a2f83baabf2b38229c2d00a2f4f6f3
jakebulling <jakebulling@gmail.com>
2021-12-07 16:45:58 -0600

    Commit I got the tabs working and now we need to add new models.

--- FootballPredictionApp/app.R
+++ FootballPredictionApp/app.R
@@ -13,52 +13,70 @@ library(shinydashboard)
 model <- readRDS("quarter_model.rds")
 
 
+header<-dashboardHeader(title = "Win Probability Predictions")
+sidebar <-dashboardSidebar(
+    sidebarMenu(
+        menuItem("dashboard" , tabName = "dashboard"),
+        menuItem("Beginning of Game", tabName = "prior", icon = icon("th")),
+        menuItem("End of First Quarter", tabName = "first"), 
+        menuItem("End of Second Quarter", tabName = "second"), 
+        menuItem("End of Third Quarter", tabName = "third")
+    )
+)
+
+body <-dashboardBody(
+    tabItems(
+        # First tab content
+        tabItem(tabName = "dashboard",
+                h2("We should put an introduction section here and explain our methodology and shit.")
+        ),
+        
+        
+        # Second tab content
+        tabItem(tabName = "prior",
+                h2('Priors Soon')
+        ),
+        
+        tabItem(tabName = "first", 
+                h2("Model coming soon")
+        ), 
+        
+        tabItem(tabName = "second", 
+                h2("Hopefully this works")
+        ), 
+        tabItem(tabName = "third", 
+                box(tableOutput('binary_prediction')),
+                box(title = 'inputs',
+                    sliderInput(inputId = "score_differential.x",
+                                label = "Score Differential 1st",
+                                min = -30,
+                                max = 30,
+                                value = 0),
+                    sliderInput(inputId = "score_differential.y",
+                                label = "Score Differential 2nd",
+                                min = -30,
+                                max = 30,
+                                value = 0),
+                    sliderInput(inputId = "score_differential.x.x",
+                                label = "Score Differential 3rd",
+                                min = -30,
+                                max = 30,
+                                value = 0)
+                )
+        )
+    )
+)
+
+ui <-dashboardPage(header, sidebar, body)
+    
+
 server <- function(input, output) {
     output$binary_prediction <- renderTable(posterior_predict(model, newdata = data.frame(score_differential.x = input$score_differential.x,
                                                                                           score_differential.y = input$score_differential.y,
                                                                                           score_differential.x.x = input$score_differential.x.x)) %>%
                                                 as.data.frame(.) %>%
-                                                summarise(Prob = mean(`1`)))
-    }
-
-ui <- dashboardPage(
-    dashboardHeader(title = "Win Probability Predictions"),
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("dashboard" , tabName = "dashboard"),
-            menuItem("Start of Game", tabName = "Start of Game", icon = icon("th"))
-        )
-    ),
-    dashboardBody(
-        tabItems(
-            # First tab content
-            tabItem(tabName = "dashboard",
-                    box(tableOutput('binary_prediction')),
-                    box(title = 'inputs',
-                        sliderInput(inputId = "score_differential.x",
-                                                 label = "Score Differential 1st",
-                                                 min = -30,
-                                                 max = 30,
-                                                 value = 0),
-                        sliderInput(inputId = "score_differential.y",
-                                                 label = "Score Differential 2nd",
-                                                 min = -30,
-                                                 max = 30,
-                                                 value = 0),
-                        sliderInput(inputId = "score_differential.x.x",
-                                                 label = "Score Differential 3rd",
-                                                 min = -30,
-                                                 max = 30,
-                                                 value = 0),
-                                 )),
-            
-            # Second tab content
-            tabItem(tabName = "Start of Game",
-                    box(title = 'Priors Soon'))
-    ))
-)
-
-
+                                               summarise(Prob = mean(`1`)))
+}
 # Run the application
 shinyApp(ui = ui, server = server)
 
