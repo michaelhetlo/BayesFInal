0d237161ace3760687a4a5a4ddc11d74a37378c7
michaelhelto <mhelton@macalester.edu>
2021-12-09 11:07:00 -0600

    all models in

--- FootballPredictionApp/app.R
+++ FootballPredictionApp/app.R
@@ -10,7 +10,10 @@ library(lubridate)
 library(nflfastR)
 library(shinydashboard)
 
-model3rd <- readRDS("quarter_model.rds")
+modelpre <- readRDS('pregame_model.rds')
+model1st <- readRDS('first_quarter.rds')
+model2nd <- readRDS('second_quarter.rds')
+model3rd <- readRDS("third_quarter.rds")
 
 ui <- dashboardPage(
     dashboardHeader(title = "Win Probability Predictions"),
@@ -28,11 +31,23 @@ ui <- dashboardPage(
                     box(title = 'Welcome Page')),
             # Second tab content
             tabItem(tabName = "StartofGame",
-                    box(title = 'Prediction Start of Game')),
+                    box(title = 'Prediction Start of Game'),
+                    box(tableOutput('pregame_prediction')),
+                    sliderInput(inputId = "spread_line_pre",
+                                label = "Home Team Spread",
+                                min = -21,
+                                max = 21,
+                                value = 0)),
             # Third tab content
             tabItem(tabName = "FirstQuarter",
                     box(title = 'Prediction After First Quarter'),
-                    sliderInput(inputId = "",
+                    box(tableOutput('firstquarter_prediction')),
+                    sliderInput(inputId = "spread_line_1",
+                                label = "Home Team Spread",
+                                min = -21,
+                                max = 21,
+                                value = 0),
+                    sliderInput(inputId = "scoredif1.1st",
                                 label = "Score Differential 1st",
                                 min = -30,
                                 max = 30,
@@ -40,12 +55,18 @@ ui <- dashboardPage(
             # Fourth tab content
             tabItem(tabName = "SecondQuarter",
                     box(title = 'Prediction After Second Quarter'),
-                    sliderInput(inputId = "",
+                    box(tableOutput('secondquarter_prediction')),
+                    sliderInput(inputId = "spread_line_2",
+                                label = "Home Team Spread",
+                                min = -21,
+                                max = 21,
+                                value = 0),
+                    sliderInput(inputId = "scoredif1.2nd",
                                 label = "Score Differential 1st",
                                 min = -30,
                                 max = 30,
                                 value = 0),
-                    sliderInput(inputId = "",
+                    sliderInput(inputId = "scoredif2.2nd",
                                 label = "Score Differential 2nd",
                                 min = -30,
                                 max = 30,
@@ -54,6 +75,11 @@ ui <- dashboardPage(
             tabItem(tabName = "ThirdQuarter",
                     box(title = 'Prediction After Third Quarter'),
                     box(tableOutput('binary_prediction')),
+                    sliderInput(inputId = "spread_line_3",
+                                label = "Home Team Spread",
+                                min = -21,
+                                max = 21,
+                                value = 0),
                     sliderInput(inputId = "score_differential.x",
                                 label = "Score Differential 1st",
                                 min = -30,
@@ -74,9 +100,22 @@ ui <- dashboardPage(
 server <- function(input, output) {
     output$binary_prediction <- renderTable(posterior_predict(model3rd, newdata = data.frame(score_differential.x = input$score_differential.x,
                                                                                           score_differential.y = input$score_differential.y,
-                                                                                          score_differential.x.x = input$score_differential.x.x)) %>%
+                                                                                          score_differential.x.x = input$score_differential.x.x,
+                                                                                          spread_line.x = -(input$spread_line_3))) %>%
                                                 as.data.frame(.) %>%
                                                summarise(Prob = mean(`1`)))
+    output$pregame_prediction <- renderTable(posterior_predict(modelpre, newdata = data.frame(spread_line.x = -(input$spread_line_pre))) %>%
+                                                 as.data.frame(.) %>%
+                                                 summarise(Prob = mean(`1`)))
+    output$firstquarter_prediction <- renderTable(posterior_predict(model1st, newdata = data.frame(spread_line.x = -(input$spread_line_1),
+                                                                                                   score_differential.x = input$scoredif1.1st)) %>%
+                                                      as.data.frame(.) %>%
+                                                      summarise(Prob = mean(`1`)))
+    output$secondquarter_prediction <- renderTable(posterior_predict(model1st, newdata = data.frame(spread_line.x = -(input$spread_line_2),
+                                                                                                   score_differential.x = input$scoredif1.2nd,
+                                                                                                   score_differential.y = input$scoredif2.2nd)) %>%
+                                                      as.data.frame(.) %>%
+                                                      summarise(Prob = mean(`1`)))
 }
 # Run the application
 shinyApp(ui = ui, server = server)
